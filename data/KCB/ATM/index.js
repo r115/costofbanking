7599d9417f92e03074bf0b3a63ea9aab70e3a5fa
Bernard Nandwa <bernard.nandwa@andela.com>
2020-12-09 12:58:19 +0300

    Update styling for the landing page

---
+++ data/KCB/ATM/index.js
@@ -0,0 +1,12 @@
+let atmCharges;
+
+export default atmCharges = [
+  {
+    effectiveDate: '18-08-2020',
+    customer: true,
+    amount: 36,
+    currency: 'kes',
+    maxAmount: 40000,
+    minAmount: 100
+  }
+]
--- pages/index.tsx
+++ pages/index.tsx
@@ -8,9 +8,17 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-          <div>
-              <div>//cost.of.banking</div>
+      <main className={'w-full h-full flex flex-row'}>
+          <div className={'w-1/5 min-h-screen bg-black px-3'}>
+              <div className={'text-yellow-200 pt-4 font-mono text-lg'}>
+                  //cost.of.banking
+              </div>
+              <div className={'text-yellow-200 pt-2 font-mono text-xs'}>
+                  Banking tariffs comparison engine.
+              </div>
+          </div>
+          <div className={'w-4/5'}>
+              <div>Pick a transaction to see alternatives</div>
           </div>
 
       </main>
